881469a9302f5b9a9a8fc69f5a7b9af6e665d162
abhilashchinthakayala <abhilashch@testvagrant.com>
2022-01-20 15:43:05 +0530

    added Create User test

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,6 +13,27 @@ public class SampleTests {
                 .then()
                     .statusCode(200)
                     .log().body();
+    }
+    @Test
+    public void shouldCreateUser (){
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer a1a2a6234bc45c474a967ea6be3b56af091df98c272d3823ad41db39a2428790")
+                .body("{\n" +
+                        "    \"name\": \"Tenali Ramakrishna\",\n" +
+                        "    \"gender\": \"male\",\n" +
+                        "    \"email\": \"tenali.ramakrishna3@email.com\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v1/users")
+                .then()
+                    .log().body()
+                    .statusCode(201);
+
+
+
     }
 
 }
