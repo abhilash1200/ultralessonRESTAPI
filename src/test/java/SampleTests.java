c09854cb4d6fc7c8e8caee39441fa6d3626aa7a0
abhilashchinthakayala <abhilashch@testvagrant.com>
2022-01-20 13:10:29 +0530

    Initial commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+    @Test
+    public void shouldGetNumber(){
+        Assert.assertEquals(20,20);
+
+    }
+
+}
